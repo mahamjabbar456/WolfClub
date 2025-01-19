bc6561914ede99921265d818dfbca38733937857
mahamjabbar456 <mahamjabbar320@gmail.com>
2025-01-19 19:04:35 +0500

    Wolf Club website made with the help of html,css and javascript

---
+++ script.js
@@ -0,0 +1,128 @@
+var cursorElement = document.querySelector(".cursor");
+var BlurElement = document.querySelector(".cursor-blur");
+document.addEventListener("mousemove",function(dets){
+    cursorElement.style.left = dets.x +"px";
+    cursorElement.style.top = dets.y +"px";
+    BlurElement.style.left = dets.x - 250 +"px";
+    BlurElement.style.top = dets.y - 250+"px";
+})
+
+var h4all = document.querySelectorAll("#nav h4");
+h4all.forEach(function(elem){
+    elem.addEventListener("mouseenter",function(){
+        cursorElement.style.scale = 3;
+        cursorElement.style.border = "1px solid #fff";
+        cursorElement.style.backgroundColor = "transparent"
+    })
+    elem.addEventListener("mouseleave",function(){
+        cursorElement.style.scale = 1;
+        cursorElement.style.border = "0px solid #95c11e";
+        cursorElement.style.backgroundColor = "#95c11e"
+    })
+})
+
+gsap.to("#nav",{
+    backgroundColor:"#000",
+    height:"180px",
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        // markers:true,
+        start:"top -10%",
+        end:"top -11%",
+        scrub:1
+    }
+})
+
+gsap.to(".main",{
+    backgroundColor:"#000",
+    scrollTrigger:{
+        trigger:".main",
+        scroller:"body",
+        // markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scurb:2
+    }
+})
+
+gsap.from(".about-us img,.about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    stagger:0.4,
+    scrollTrigger:{
+        trigger:".about-us",
+        scroller:"body",
+        start:"top 60%",
+        end:"top 58%",
+        scurb:3
+    }
+})
+
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+    // stagger:0.1,
+    scrollTrigger:{
+        trigger:".card",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
+        scurb:2
+    }
+})
+
+gsap.from("#colon1",{
+    y:-70,
+    x:-70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        // markers:true,
+        start:"top 50%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+
+gsap.from("#colon2",{
+    y:70,
+    x:70,
+    scrollTrigger:{
+        trigger:"#colon1",
+        scroller:"body",
+        // markers:true,
+        start:"top 50%",
+        end:"top 45%",
+        scrub:4
+    }
+})
+
+
+gsap.from(".elem",{
+    x:-50,
+    opacity:0,
+    duration:1,
+    // stagger:0.1,
+    scrollTrigger:{
+        trigger:".elem",
+        scroller:"body",
+        start:"top 70%",
+        end:"top 65%",
+        scurb:2
+    }
+})
+
+gsap.from(".page4 h1",{
+    y:50,
+    scrollTrigger:{
+        trigger:"#page4 h1",
+        scroller:"body",
+        start:"top 75%",
+        end:"top 70%",
+        scrub:4
+    }
+})
